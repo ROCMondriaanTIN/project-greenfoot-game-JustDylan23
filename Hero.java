dbad42964335873bae28000ae9a628d0408a25f8
JustDylan23 <dylanvdhout@gmail.com>
2018-11-15 11:04:49 +0100

    Updated Hero Animation

--- Hero.java
+++ Hero.java
@@ -2,7 +2,6 @@
 import greenfoot.*;
 
 /**
- *
  * @author R. Springer
  */
 public class Hero extends Mover {
@@ -10,20 +9,33 @@ public class Hero extends Mover {
     private final double gravity;
     private final double acc;
     private final double drag;
+
+    //hero stats
     private int heroState = 1;
+    private boolean isOnGround;
+    private boolean isStandingStill;
+    private boolean isWalking;
+    private double walkState = 0;
+    private boolean direction = true;
+
+    private double posToNeg(double x) {
+        return x - x * 2;
+    }
 
     public Hero() {
         super();
         gravity = 9.8;
         acc = 0.6;
         drag = 0.8;
-        setHeroState("p" + heroState + "_front");
+        setTexture("p" + heroState + "_front");
     }
 
     @Override
     public void act() {
-        handleInput();
-        
+        handleAnimation();
+        handleMovement();
+        handleHeroStats();
+
         velocityX *= drag;
         velocityY += acc;
         if (velocityY > gravity) {
@@ -39,21 +51,121 @@ public class Hero extends Mover {
         }
     }
 
-    public void handleInput() {
-        if (Greenfoot.isKeyDown("w")) {
-            velocityY = -12.5;
-        }
+    private void handleAnimation() {
+        handleAmbientAnimation();
+        handleWalingAnimation();
+        handleInAirAnimation();
+    }
 
-        if (Greenfoot.isKeyDown("a")) {
-            velocityX = -6;
-        } else if (Greenfoot.isKeyDown("d")) {
-            velocityX = 6;
+    private void handleMovement() {
+        horizontalMovementHandler();
+        jumpingHandler();
+    }
+
+    private void handleHeroStats() {
+        updateOnGroundStats();
+        updateIsWalking();
+        updateDirection();
+        updateIsStandingStill();
+    }
+
+    private void handleInAirAnimation() {
+        if (velocityY < 0 && !isOnGround) {
+            setTextureWithDirection("p" + heroState + "_jump");
         }
     }
 
-    private void setHeroState(String texture) {
+    private void handleAmbientAnimation() {
+        if (isStandingStill) {
+            setTexture("p" + heroState + "_front");
+        }
+    }
+
+    private void handleWalingAnimation() {
+        if (isWalking) {
+            walkState += 0.25;
+            setTextureWithDirection("p" + heroState + "_walk\\p" + heroState + "_walk" + (int) Math.ceil(walkState));
+            if (walkState == 11) walkState = 0;
+        } else walkState = 0;
+    }
+
+    private void jumpingHandler() {
+        if (isOnGround) {
+            if (Greenfoot.isKeyDown("space")) {
+                velocityY = -12.5;
+            }
+        }
+    }
+
+    private void horizontalMovementHandler() {
+        if (Greenfoot.isKeyDown("a") && Greenfoot.isKeyDown("d")) return;
+        if (Greenfoot.isKeyDown("a")) velocityX = posToNeg(6);
+        if (Greenfoot.isKeyDown("d")) velocityX = 6;
+    }
+
+    private void updateOnGroundStats() {
+        int width = getImage().getWidth() / 2;
+
+        Boolean successRate = false;
+
+        for (Tile tile : getObjectsAtOffset(0, getImage().getHeight() / 2 + 1, Tile.class)) {
+            if (tile.isSolid) successRate = true;
+        }
+        if (!successRate) {
+            for (Tile tile : getObjectsAtOffset(width - 3, getImage().getHeight() / 2 + 1, Tile.class)) {
+                if (tile.isSolid) successRate = true;
+            }
+            if (!successRate) {
+                for (Tile tile : getObjectsAtOffset((int) posToNeg(width) + 3, getImage().getHeight() / 2 + 1, Tile.class)) {
+                    if (tile.isSolid) successRate = true;
+                }
+            }
+        }
+
+        for (Tile tile : getObjectsAtOffset(0, getImage().getHeight() / 2 + 1, Tile.class)) {
+            if (tile.isSolid) successRate = true;
+            break;
+        }
+        isOnGround = successRate;
+    }
+
+    private void updateIsWalking() {
+        if (Math.abs(velocityX) > 0.5 && isOnGround) {
+            isWalking = true;
+        } else isWalking = false;
+    }
+
+    private void updateDirection() {
+        if (velocityX > 0) {
+            if (!direction) {
+                direction = true;
+                getImage().mirrorHorizontally();
+            }
+        }
+        if (velocityX < 0) {
+            if (direction) {
+                direction = false;
+                getImage().mirrorHorizontally();
+            }
+        }
+    }
+
+    private void updateIsStandingStill() {
+        if (Math.abs(velocityX) < 0.4 && isOnGround) {
+            isStandingStill = true;
+        } else isStandingStill = false;
+    }
+
+    private void setTexture(String texture) {
         setImage("Player\\" + texture + ".png");
         getImage().scale(60, 81);
+    }
+
+    private void setTextureWithDirection(String texture) {
+        setImage("Player\\" + texture + ".png");
+        getImage().scale(60, 81);
+        if (!direction) getImage().mirrorHorizontally();
+
     }
 
     public int getWidth() {
